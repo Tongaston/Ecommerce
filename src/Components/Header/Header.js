4acbed390dea015c92442c40f204b1f9249a6127
Gaston Moure <gaston.moure@gmail.com>
2025-04-09 18:33:02 +0200

    change letters to links

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -7,11 +7,11 @@ export function createHeader() {
   <a href="#" class='logo'><img src="../public/img/logo-volcanica-store.png"></a>
   <nav>
     <ul id="navbar">
-      <li><a class="active" href="index.html">Home</a></li>
-      <li><a href="shop.html">Shop</a></li>
+      <li><a class="active" href="index.html">Inicio</a></li>
+      <li><a href="shop.html">Tienda</a></li>
       <li><a href="blog.html">Blog</a></li>
-      <li><a href="about.html">About</a></li>
-      <li><a href="contact.html">Contact</a></li>
+      <li><a href="about.html">Sobre nosotros</a></li>
+      <li><a href="contact.html">Contacto</a></li>
       <li><a href="cart.html"><i class="bi bi-cart-fill"></i></a></li>
     </ul>
   </nav>
