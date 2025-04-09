0153ec90dc34bcd51f012c2d5a573ec745d3e7c6
Gaston Moure <gaston.moure@gmail.com>
2025-04-09 18:32:23 +0200

    import and add Intro component

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import './style.css'
 import { createHeader } from './Components/Header/Header.js'
+import { createIntro } from './Components/Intro/Intro.js'
 
 document.addEventListener('DOMContentLoaded', () => {
-  const app = document.querySelector('#app') // Suponiendo que hay un <div id="app"></div>
+  const app = document.querySelector('#app')
   app.appendChild(createHeader())
+  app.appendChild(createIntro())
 })
