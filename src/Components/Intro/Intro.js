a897ae6804d9b5c2bd88d8d751775601cea87925
Gaston Moure <gaston.moure@gmail.com>
2025-05-19 14:49:56 +0200

    add intro component

---
+++ src/Components/Intro/Intro.js
@@ -0,0 +1,23 @@
+import './Intro.css'
+
+export function createIntro() {
+  const intro = document.createElement('section')
+  intro.innerHTML = `
+   <section id="intro">
+   <div class="contenido-texto">
+   <p>Cerveza artesanal con alma volcánica</p>
+   <h1>🌋 Bienvenidos a </h1>
+   <img class="img-intro" src="../public/img/logo-volcanica-store.png"> 
+   <div>
+   <p>Elegí. Comprá. Brindá. Todo desde tu casa.</p>
+   </div>
+   <button class="btn">Comprar!</button>
+    </div>
+    </section>
+  `
+  return intro
+}
+
+/*
+<span class="span-title-1">VOLCANICA</span> <span class="span-title-2">Store</span>
+*/
