6717c5cf19edd2fbdd150f50db0f026bd7c75323
Gaston Moure <gaston.moure@gmail.com>
2025-06-02 23:47:41 +0200

    add features component

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 import './style.css'
 import { createHeader } from './Components/Header/Header.js'
 import { createIntro } from './Components/Intro/Intro.js'
+import { createFeatures } from './Components/Features/features.js'
 
 document.addEventListener('DOMContentLoaded', () => {
   const app = document.querySelector('#app')
   app.appendChild(createHeader())
   app.appendChild(createIntro())
+  app.appendChild(createFeatures())
 })
