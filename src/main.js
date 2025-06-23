896b3ad762a0871fcc397ea66230be965aac2978
Gaston Moure <gaston.moure@gmail.com>
2025-06-23 15:10:14 +0200

    add Favs component

--- src/main.js
+++ src/main.js
@@ -2,10 +2,12 @@ import './style.css'
 import { createHeader } from './Components/Header/Header.js'
 import { createIntro } from './Components/Intro/Intro.js'
 import { createFeatures } from './Components/Features/features.js'
+import { createFavs } from './Components/Favs/Favs.js'
 
 document.addEventListener('DOMContentLoaded', () => {
   const app = document.querySelector('#app')
   app.appendChild(createHeader())
   app.appendChild(createIntro())
   app.appendChild(createFeatures())
+  app.appendChild(createFavs())
 })
