a9d6eacb7df8ff0b51df05a4bc14d629a7c2d3db
Gaston Moure <gaston.moure@gmail.com>
2025-07-02 20:06:15 +0200

    add createBanner component

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import { createHeader } from './Components/Header/Header.js'
 import { createIntro } from './Components/Intro/Intro.js'
 import { createFeatures } from './Components/Features/features.js'
 import { createFavs } from './Components/Favs/Favs.js'
+import { createBanner } from './Components/Banner/Banner.js'
 
 document.addEventListener('DOMContentLoaded', () => {
   const app = document.querySelector('#app')
@@ -10,4 +11,5 @@ document.addEventListener('DOMContentLoaded', () => {
   app.appendChild(createIntro())
   app.appendChild(createFeatures())
   app.appendChild(createFavs())
+  app.appendChild(createBanner())
 })
